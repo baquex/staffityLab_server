d2c8cb50732e6ebc9d9ed26db6953d426bbad8d1
Miguel <miguel.cuevas18@gmail.com>
2020-07-21 17:18:46 -0500

    connected server with hostGator DB

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+
+
+
+/***   CHANGE THE PORT TO 8080 FOR PROD */
+const port = process.env.PORT || 3000;
+
+
 
 app.get('/', (req, res)=>{
 
   console.log('request received: '+JSON.stringify(req.headers));
+  
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: '162.241.61.133',
+    user: 'staffity_root',
+    password: 'scammacs',
+    database: 'staffity_db'
+  });
+  
+  connection.connect()
+  
+  connection.query('SELECT * FROM CONTACT_FORM', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log('The solution is: ', rows)
 
-  res.write('test this...');
-  res.send('hello world!');
+    res.send(rows);
+  });
+  
+  connection.end();
+
 
 });
+
+/*
+
+app.post('/api/contact-form', (req, res)=>{
+
+});*/
 
 app.listen(port, ()=> console.log('the port is:'+port));
